71c00f18b6ff07449ff4458159e3a029e30401e8
MarcoSilva0 <marcoantonio02016@outlook.com>
2023-12-31 18:49:21 -0300

    feat: Open/closed principle

---
+++ src/ocp/classes/Order.ts
@@ -0,0 +1,34 @@
+import { Messaging } from '../services/Messaging';
+import { Persistency } from '../services/Persistency';
+import { ShoppingCart } from './ShoppingCart';
+import { OrderStatus } from './interfaces/order-status';
+
+export class Order {
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+  ) {}
+
+  private _orderStatus: OrderStatus = 'open';
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+
+    this.messaging.sendMenssage(
+      `Seu pedido com total de ${this.cart.totalWithDiscount()} foi recebido`,
+    );
+
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
---
+++ src/ocp/main.ts
@@ -0,0 +1,27 @@
+/*
+Open/closed principle
+Entidades devem estar abertas para extensão, mas fechadas para modificação.
+*/
+import { Messaging } from './services/Messaging';
+import { Order } from './classes/Order';
+import { Persistency } from './services/Persistency';
+import { Product } from './classes/Product';
+import { ShoppingCart } from './classes/ShoppingCart';
+import { NonePercentDiscount } from './classes/Discount';
+
+const noneDiscount = new NonePercentDiscount();
+const shoppingCart = new ShoppingCart(noneDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Shirt', 10.29, 1));
+shoppingCart.addItem(new Product('Shoes', 39.12, 1));
+shoppingCart.addItem(new Product('Hat', 1.99, 1));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
